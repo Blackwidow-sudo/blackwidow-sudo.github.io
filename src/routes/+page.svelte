b693cb2cf8f7ffb62c0827ab309391b32d32740b
Blackwidow-sudo <bob_merlin92@web.de>
2025-01-10 21:26:43 +0100

    fix: show link to github for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://github.com/Blackwidow-sudo">Blackwidow-sudo</a> to see more of my projects.
+</p>
